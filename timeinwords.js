5a0f0e0b707b49cec43b69ef2656c634f50035e5
mimi klein <lifeistoogoodtobetrue@gmail.com>
2017-06-14 09:33:45 -0400

    fix indent, changed let to const

--- timeinwords.js
+++ timeinwords.js
@@ -1,6 +1,6 @@
 // Given the time in numerals we may convert it into words, as shown below:
 
-// 5:00 -> five o' clock 
+// 5:00 -> five o'clock 
 // 5:01 -> one minute past five
 // 5:10 -> ten minutes past five
 // 5:15 -> quarter past five
@@ -12,72 +12,72 @@
 // Write a program which prints the time in words for the input given in the format mentioned above.
 
 function timeInWords (time) {
-  let h = time.match(/^.*(?=(\:))/g).join();//match everything before the ":" to get the hours
-  let timeArr = time.split('');
-  let colonIndex = timeArr.indexOf(':');//find the index of the ":"
-  let minuteArr = [];
-  for (i = colonIndex + 1; i < timeArr.length; i ++) { //loop through everything after the colon
+  const h = parseInt(time.match(/^.*(?=(\:))/g).join());//match everything before the ":" to get the hours
+  const timeArr = time.split('');
+  const colonIndex = timeArr.indexOf(':');//find the index of the ":"
+  const minuteArr = [];
+  for (let i = colonIndex + 1; i < timeArr.length; i ++) { //loop through everything after the colon
     minuteArr.push(timeArr[i]);//push into a minute array
-    }
-    let m = minuteArr.join('');//join the minute array to get the minutes
-    let timeConvert = {
-      1 : 'one',
-      2 : 'two',
-      3 : 'three',
-      4 : 'four',
-      5 : 'five',
-      6 : 'six',
-      7 : 'seven',
-      8 : 'eight',
-      9 : 'nine',
-      10: 'ten',
-      11: 'eleven',
-      12: 'twelve',
-      13: 'thirteen',
-      14: 'fourteen',
-      16: 'sixteen',
-      17: 'seventeen',
-      18: 'eighteen',
-      19: 'nineteen',
-      20: 'twenty',
-      21: 'twenty one',
-      22: 'twenty two',
-      23: 'twenty three',
-      24: 'twenty four',
-      25: 'twenty five',
-      26: 'twenty six',
-      27: 'twenty seven',
-      28: 'twenty eight',
-      29: 'twenty nine'
-    }
-    if (parseInt(m) <=30) {
-      hourInWords = timeConvert[parseInt(h)];
-    } else if (parseInt(m) > 30 && parseInt(h) !== 12){
-      hourInWords = timeConvert[parseInt(h) + 1];
-    } else if (parseInt(m) > 30 && parseInt(h) === 12) {
-      hourInWords = timeConvert[1];
-    }
-    if (parseInt(m) < 30) {
-      minutesInWords = timeConvert[parseInt(m)];
-    } else if (parseInt(m) > 30) {
-      minutesInWords = timeConvert[60 - parseInt(m)];
-    }
-    if (parseInt(m) === 0) {
-      timeString = `${hourInWords} o' clock`;
-    } else if (parseInt(m) === 15) {
-      timeString = `quarter past ${hourInWords}`;
-    } else if (parseInt(m) === 30) {
-      timeString = `half past ${hourInWords}`;
-    } else if (parseInt(m) === 45) {
-      timeString = `quarter to ${hourInWords}`;
-    } else if (parseInt(m) === 1) {
-      timeString = `one minute past ${hourInWords}`;
-    } else if (parseInt(m) < 30) {
-      timeString = `${minutesInWords} minutes past ${hourInWords}`;
-    } else if (parseInt(m) === 59) {
-      timeString = `one minute to ${hourInWords}`;
-    } else if (parseInt(m) > 30) {
-      timeString = `${minutesInWords} minutes to ${hourInWords}`;
-    }
-    console.log(timeString);
+  }
+  const m = parseInt(minuteArr.join(''));//join the minute array to get the minutes
+  const timeConvert = {
+    1 : 'one',
+    2 : 'two',
+    3 : 'three',
+    4 : 'four',
+    5 : 'five',
+    6 : 'six',
+    7 : 'seven',
+    8 : 'eight',
+    9 : 'nine',
+    10: 'ten',
+    11: 'eleven',
+    12: 'twelve',
+    13: 'thirteen',
+    14: 'fourteen',
+    16: 'sixteen',
+    17: 'seventeen',
+    18: 'eighteen',
+    19: 'nineteen',
+    20: 'twenty',
+    21: 'twenty one',
+    22: 'twenty two',
+    23: 'twenty three',
+    24: 'twenty four',
+    25: 'twenty five',
+    26: 'twenty six',
+    27: 'twenty seven',
+    28: 'twenty eight',
+    29: 'twenty nine'
+  }
+  if (m <=30) {
+    hourInWords = timeConvert[h];
+  } else if (m > 30 && h !== 12){
+    hourInWords = timeConvert[h + 1];
+  } else if (m > 30 && h === 12) {
+    hourInWords = timeConvert[1];
+  }
+  if (m < 30) {
+    minutesInWords = timeConvert[m];
+  } else if (m > 30) {
+    minutesInWords = timeConvert[60 - m];
+  }
+  if (m === 0) {
+    timeString = `${hourInWords} o' clock`;
+  } else if (m === 15) {
+    timeString = `quarter past ${hourInWords}`;
+  } else if (m === 30) {
+    timeString = `half past ${hourInWords}`;
+  } else if (m === 45) {
+    timeString = `quarter to ${hourInWords}`;
+  } else if (m === 1) {
+    timeString = `one minute past ${hourInWords}`;
+  } else if (m < 30) {
+    timeString = `${minutesInWords} minutes past ${hourInWords}`;
+  } else if (m === 59) {
+    timeString = `one minute to ${hourInWords}`;
+  } else if (m > 30) {
+    timeString = `${minutesInWords} minutes to ${hourInWords}`;
+  }
+  console.log(timeString);
 }
